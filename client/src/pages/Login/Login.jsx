2df21e7040a20317aadb2ef2d9b93126f02feea2
Jérémie Desnoes <j.desnoes1@gmail.com>
2023-05-12 22:20:47 +0200

    Début page connexion

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,7 +1,25 @@
 import "./login.scss";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h3>Salut à tous !</h3>
+          <p>
+            Bienvenue sur Formation Dev ! Ici, on propose beaucoup de formations
+            différentes dans le developpement Web, principalement sur les
+            technologies Javascript
+          </p>
+          <span>Vous n'avez pas de compte ?</span>
+          <button>Créer un compte</button>
+        </div>
+        <div className="right">
+          <h3>Connexion</h3>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
