587d72933ba09ae139cfe1c5b17e46af099ebcd7
Jérémie Desnoes <j.desnoes1@gmail.com>
2023-05-13 09:24:04 +0200

    Reglage NavBar

--- client/src/composants/Navbar/Navbar.jsx
+++ client/src/composants/Navbar/Navbar.jsx
@@ -9,18 +9,18 @@ const Navbar = () => {
         <div className="logo">
           <LogoDevIcon />
         </div>
-        <Link to="/">
+        <Link to="/" className="link">
           <h3>Formations Dev</h3>
         </Link>
       </div>
       <div className="right">
-        <Link to="/connexion">
+        <Link to="/connexion" className="link">
           <p>Connexion</p>
         </Link>
-        <Link to="/formations">
+        <Link to="/formations" className="link">
           <p>Nos formations</p>
         </Link>
-        <Link to="/about">
+        <Link to="/about" className="link">
           <p>A propos de nous</p>
         </Link>
       </div>
