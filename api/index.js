c9ac7fc548773c0d585fcdaefe6a7c9a301c1bb8
Jérémie Desnoes <j.desnoes1@gmail.com>
2023-05-13 10:28:47 +0200

    Verif connexion db

--- api/index.js
+++ api/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import authRoutes from "./routes/auths.js";
+import { db } from "./db.js";
 
 const app = express();
 const port = 8500;
 
-app.get("/", (req, res) => res.send("Hello ! Express comment ça va?"));
+db.connect(function (err) {
+  if (err) throw err;
+  console.log("Connecté à la base de données MySQL!");
+});
 
 app.listen(port, () =>
   console.log(`L'application est lancée sur : http://localhost:${port}`)
