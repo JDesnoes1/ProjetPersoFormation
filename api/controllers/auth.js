1e798aab8530417a50ee08221368f469bf4dd65c
Jérémie Desnoes <j.desnoes1@gmail.com>
2023-05-13 11:07:06 +0200

    Register fini en back, test insomnia bon

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,1 +1,36 @@
-export const register = (req, res) => {};
+import { db } from "../db.js";
+import bcrypt from "bcryptjs";
+
+export const register = (req, res) => {
+  if (!req.body || !req.body.username) {
+    return res.status(400).json("Le nom d'utilisateur est requis !");
+  }
+  //Vérifier si l'utilisateur existe
+  const q = "SELECT * FROM users WHERE username = ?";
+
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) return res.status(500).json(err);
+    if (data.length) return res.status(409).json("L'utilisateur existe déjà !");
+
+    //Si l'utilisateur n'est pas présent en base de données, on peut donc créer un nouvel utilisateur.
+    //On oublie pas de hash le password avec bcryptjs
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
+    const q =
+      "INSERT INTO users (`username`, `email`, `nom`, `prenom`, `ville`, `mdp`) VALUES(?)";
+
+    const values = [
+      req.body.username,
+      req.body.email,
+      req.body.nom,
+      req.body.prenom,
+      req.body.ville,
+      hashedPassword,
+    ];
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("L'utilisateur a bien été crée");
+    });
+  });
+};
--- api/index.js
+++ api/index.js
@@ -5,6 +5,11 @@ import { db } from "./db.js";
 const app = express();
 const port = 8500;
 
+//Middleware
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
 db.connect(function (err) {
   if (err) throw err;
   console.log("Connecté à la base de données MySQL!");
