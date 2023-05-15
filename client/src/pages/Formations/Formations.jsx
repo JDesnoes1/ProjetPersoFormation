9764840b041c2bbffd6ebeb124ee53996116a378
Jérémie Desnoes <j.desnoes1@gmail.com>
2023-05-15 09:18:57 +0200

    Style button formation

--- client/src/pages/Formations/Formations.jsx
+++ client/src/pages/Formations/Formations.jsx
@@ -5,13 +5,26 @@ const Formations = () => {
     <div className="formations">
       <div className="cardsContainer">
         <div className="card card1">
-          <h2>Pack Back Node Js</h2>
+          <h2>Pack BackEnd</h2>
+          <p>
+            Prix: <span>550€</span>
+          </p>
+          <button>Découvrir</button>
         </div>
         <div className="card card2">
-          <h2>Pack FullStack Node React</h2>
+          <h2>Pack FullStack</h2>
+          <p>Meilleure vente !</p>
+          <p>
+            Prix: <span>1050€</span> 899.99€
+          </p>
+          <button>Découvrir</button>
         </div>
         <div className="card card3">
-          <h2>Pack Front React Js</h2>
+          <h2>Pack FrontEnd</h2>
+          <p>
+            Prix: <span>400€</span>
+          </p>
+          <button>Découvrir</button>
         </div>
       </div>
     </div>
