bc357c712f4a903b8b62969ecd64b6e8075875f5
Jérémie Desnoes <j.desnoes1@gmail.com>
2023-05-24 22:32:24 +0200

    Gestions des formations affichés avec gratuit

--- client/src/pages/Cours/Cours.jsx
+++ client/src/pages/Cours/Cours.jsx
@@ -21,27 +21,58 @@ const Cours = () => {
     getAllAchats();
   }, []);
 
+  const formationIds =
+    mesFormations &&
+    mesFormations.map((maFormation) => maFormation.id_formation);
+  const hasFullStack = formationIds && formationIds.includes(3);
+
   return (
     <div className="cours">
       <div className="card">
         <div className="infos">
           <h1>Mes formations :</h1>
           {mesFormations.length >= 1 ? (
-            <h2>Vous possédez {mesFormations.length} formation(s)</h2>
+            hasFullStack ? (
+              <h2>Vous possédez {mesFormations.length} formation(s)</h2>
+            ) : (
+              <>
+                <h2>Vous possédez {mesFormations.length} formation(s)</h2>
+                <h2>Accès limité aux 2 modules offerts</h2>
+              </>
+            )
           ) : (
-            <h2>Accès limité aux modules offerts</h2>
+            <h2>Accès limité aux 2 modules offerts</h2>
           )}
         </div>
         <div className="mesFormations">
           {mesFormations.length >= 1 ? (
-            mesFormations.map((maFormation) => (
-              <div className="formationCards" key={maFormation.id}>
-                <p>Formation {maFormation.nom}</p>
-                <Link to={`/cours/${maFormation.id_formation}`}>
-                  <button>Accès formation {maFormation.nom}</button>
-                </Link>
-              </div>
-            ))
+            hasFullStack ? (
+              mesFormations.map((maFormation) => (
+                <div className="formationCards" key={maFormation.id}>
+                  <p>Formation {maFormation.nom}</p>
+                  <Link to={`/cours/${maFormation.id_formation}`}>
+                    <button>Accès formation {maFormation.nom}</button>
+                  </Link>
+                </div>
+              ))
+            ) : (
+              <>
+                {mesFormations.map((maFormation) => (
+                  <div className="formationCards" key={maFormation.id}>
+                    <p>Formation {maFormation.nom}</p>
+                    <Link to={`/cours/${maFormation.id_formation}`}>
+                      <button>Accès formation {maFormation.nom}</button>
+                    </Link>
+                  </div>
+                ))}
+                <div className="formationCards">
+                  <p>Accès limité module FullStack</p>
+                  <Link>
+                    <button>Accès non premium FullStack</button>
+                  </Link>
+                </div>
+              </>
+            )
           ) : (
             <div className="formationCards">
               <p>Accès limité module FullStack</p>
