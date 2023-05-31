656fc5a3eec856ece65e44b76a1eee8606a342ed
Jérémie Desnoes <j.desnoes1@gmail.com>
2023-05-31 20:00:03 +0200

    Update et delete sous_titres fonctionne Insomnia

--- api/controllers/sousTitre.js
+++ api/controllers/sousTitre.js
@@ -12,3 +12,21 @@ export const insertSousTitre = (req, res) => {
     return res.status(200).json("Succès ! Le sous-titre a bien été inséré !");
   });
 };
+
+export const updateSousTitre = (req, res) => {
+  const q = "UPDATE sous_titres set `contenu` = ?, `ordre`= ? WHERE id = ?";
+
+  db.query(q, [req.body.contenu, req.body.ordre, req.body.id], (err, data) => {
+    if (err) return res.status(500).json(err);
+    return res.status(200).json("Succès ! Le sous-titre a bien été modifié !");
+  });
+};
+
+export const deleteSousTitre = (req, res) => {
+  const q = "DELETE from sous_titres WHERE id = ?";
+
+  db.query(q, [req.body.id], (err, data) => {
+    if (err) return res.status(500).json(err);
+    return res.status(200).json("Succès ! Le paragraphe a bien été supprimé !");
+  });
+};
--- api/routes/sousTitres.js
+++ api/routes/sousTitres.js
@@ -1,8 +1,14 @@
 import Express from "express";
-import { insertSousTitre } from "../controllers/sousTitre.js";
+import {
+  insertSousTitre,
+  updateSousTitre,
+  deleteSousTitre,
+} from "../controllers/sousTitre.js";
 
 const router = Express.Router();
 
 router.post("/:moduleId", insertSousTitre);
+router.put("/", updateSousTitre);
+router.delete("/", deleteSousTitre);
 
 export default router;
